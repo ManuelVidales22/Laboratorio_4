5258afcafe58f75a137a917cef0c0f3308001700
ManuelVidales22 <manuelvidales33@gmail.com>
2023-06-17 09:18:29 -0500

    Se termino vista de Venta e ingresar Datos

---
+++ src/vista/IngresarDatos.java
@@ -0,0 +1,229 @@
+
+package vista;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+
+public class IngresarDatos extends JFrame{
+    JLabel  jlTitu,jlTitu1, jlNumIde, jlApe, jlNom, jlEdad , jlNumCelu, jlCiuRe, jlNomDulce, jlTipoDul, jlCantidad,jlprecio;
+    public JTextField jtNumIde, jtNombre, jtApellido, jtEdad;
+    public JTextField jtNomDulce;
+    public JTextField jtTipoDul, jtCantidad, jtPrecio;
+    public JTextField jtCiuRe;
+    public JTextField jtNumCelu;
+    public JComboBox<String> jcGenero ;
+    JRadioButton jr1, jr2, jr3;
+    JSpinner jsCantidad;
+    JButton jbGuardar, jbVolver, jbLimpiar1, jbLimpiar;
+    
+    public IngresarDatos(){
+        
+        setTitle("Datos");
+        setSize(700, 500);
+        getContentPane().setBackground(new Color(255, 174, 204));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        crearGUI();
+        //setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        setVisible(true);
+    }
+    
+    public void crearGUI(){
+        
+        jlTitu =  new JLabel("Datos proveedor");
+        jlTitu.setBounds(20, 10, 300, 60); 
+        //jlTitulo.setOpaque(true);// esto sirve para poner el fondo blanco del texto 
+        jlTitu.setForeground(Color.BLACK);
+        jlTitu.setFont(new Font("Arial", Font.BOLD|Font.BOLD,15)); 
+        jlTitu.setHorizontalAlignment(JLabel.CENTER);  
+        add(jlTitu);
+       
+        
+        jlNumIde =  new JLabel("Numero de idenificacion: ");
+        jlNumIde.setBounds( 30 , 60, 180, 30);
+        jlNumIde.setForeground(Color.WHITE);
+        jlNumIde.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlNumIde);
+        
+        jtNumIde =  new JTextField();
+        jtNumIde.setBounds( 200 , 60, 120, 30);
+        jtNumIde.setBorder(null);
+        add(jtNumIde);
+        
+        jlNom =  new JLabel("Nombre: ");
+        jlNom.setBounds( 30 , 100, 180, 30);
+        jlNom.setForeground(Color.WHITE);
+        jlNom.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlNom);
+        
+        jtNombre = new JTextField();
+        jtNombre.setBounds(100, 100, 170, 30);
+        jtNombre.setBorder(null);
+        add(jtNombre);   
+        
+        jlApe =  new JLabel("Apellido: ");
+        jlApe.setBounds( 30 , 140, 180, 30);
+        jlApe.setForeground(Color.WHITE);
+        jlApe.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlApe);
+        
+        jtApellido = new JTextField();
+        jtApellido.setBounds(100, 140, 170, 30);
+        jtApellido.setBorder(null);
+        add(jtApellido);
+        
+        jlEdad =  new JLabel("Edad: ");
+        jlEdad.setBounds( 30 , 180, 180, 30);
+        jlEdad.setForeground(Color.WHITE);
+        jlEdad.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlEdad);
+        
+        jtEdad = new JTextField();
+        jtEdad.setBounds(80, 180, 150, 30);
+        jtEdad.setBorder(null);
+        add(jtEdad);
+        
+        jlNumCelu =  new JLabel("Numero Telefonico: ");
+        jlNumCelu.setBounds( 30 , 220, 180, 30);
+        jlNumCelu.setForeground(Color.WHITE);
+        jlNumCelu.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlNumCelu);
+        
+        jtNumCelu = new JTextField();
+        jtNumCelu.setBounds(170, 220, 150, 30);
+        jtNumCelu.setBorder(null);
+        add(jtNumCelu);
+        
+        
+        jcGenero = new JComboBox<>();
+        jcGenero.addItem("Genero");
+        jcGenero.addItem("M"); 
+        jcGenero.addItem("F");
+        jcGenero.addItem("Otro");
+        jcGenero.setBounds(30, 260, 200, 30);  
+        setResizable(false);
+        
+        add(jcGenero);
+        
+        jlCiuRe =  new JLabel("Ciudad de recidencia: ");
+        jlCiuRe.setBounds( 30 , 300, 180, 30);
+        jlCiuRe.setForeground(Color.WHITE);
+        jlCiuRe.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlCiuRe);
+        
+        jtCiuRe = new JTextField();
+        jtCiuRe.setBounds(180, 300, 140, 30);
+        jtCiuRe.setBorder(null);
+        add(jtCiuRe);
+        
+        ////////////////////// DATOS DULCERIA //////////////////////////////
+        
+        jlTitu1 =  new JLabel("Insertar Dulces");
+        jlTitu1.setBounds(340, 10, 300, 60); 
+        //jlTitulo.setOpaque(true);// esto sirve para poner el fondo blanco del texto 
+        jlTitu1.setForeground(Color.BLACK);
+        jlTitu1.setFont(new Font("Arial", Font.BOLD|Font.BOLD,15)); 
+        jlTitu1.setHorizontalAlignment(JLabel.CENTER);  
+        add(jlTitu1); 
+        
+        
+        jlNomDulce =  new JLabel("Nombre del dulce: ");
+        jlNomDulce.setBounds( 350 , 60, 180, 30);
+        jlNomDulce.setForeground(Color.WHITE);
+        jlNomDulce.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlNomDulce);
+        
+        jtNomDulce =  new JTextField();
+        jtNomDulce.setBounds( 480 , 60, 180, 30);
+        jtNomDulce.setBorder(null);
+        add(jtNomDulce);
+        
+        
+        jlTipoDul =  new JLabel(" Categoria del dulce ");
+        jlTipoDul.setBounds( 420 , 100, 180, 30);
+        jlTipoDul.setForeground(Color.WHITE);
+        jlTipoDul.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlTipoDul);
+        
+        jr1 = new JRadioButton(" Dulce ");
+        jr1.setBounds(360, 130, 80, 30);
+        jr1.setOpaque(false);
+        add(jr1);
+        
+        jr2 = new JRadioButton(" Acido ");
+        jr2.setBounds(460, 130, 80, 30);
+        jr2.setOpaque(false);
+        add(jr2);
+        
+        jr3 = new JRadioButton(" Sin azucar ");
+        jr3.setBounds( 560, 130, 100, 30);
+        jr3.setOpaque(false);
+        add(jr3);
+        
+        
+        jlCantidad =  new JLabel(" Cantidad: ");
+        jlCantidad.setBounds( 400 , 180, 180, 30);
+        jlCantidad.setForeground(Color.WHITE);
+        jlCantidad.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlCantidad);
+        
+        
+        SpinnerNumberModel snm = new SpinnerNumberModel(0, -10, 20, 1);
+        jsCantidad =  new JSpinner(snm);
+        jsCantidad.setBounds(480, 180, 60, 30);
+        add(jsCantidad);
+        
+        jlprecio =  new JLabel(" Precio: ");
+        jlprecio.setBounds( 400 , 230, 180, 30);
+        jlprecio.setForeground(Color.WHITE);
+        jlprecio.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,13));
+        add(jlprecio);
+        
+        
+        jtPrecio =  new JTextField();
+        jtPrecio.setBounds( 460 , 230, 100, 30);
+        jtPrecio.setBorder(null);
+        add(jtPrecio);
+        
+        
+        
+        ///////////// BOTONES //////////////////////
+       
+        
+       jbGuardar = new JButton("Guardar");
+       jbGuardar.setBounds(350, 400, 100, 30);
+       add(jbGuardar);
+       
+       
+       jbVolver =  new JButton("Volver");
+       jbVolver.setBounds(230, 400, 100, 30);
+       add(jbVolver); 
+       
+       jbLimpiar =  new JButton("Limpiar");
+       jbLimpiar.setBounds(120, 350, 100, 30);
+       add(jbLimpiar);
+       
+       jbLimpiar1 =  new JButton("Limpiar");
+       jbLimpiar1.setBounds(460, 350, 100, 30);
+       add(jbLimpiar1);
+       
+       
+        
+    }
+   
+    public static void main(String[] args) {
+        IngresarDatos in =  new IngresarDatos();
+    }
+    
+}
--- src/vista/Ventana.java
+++ src/vista/Ventana.java
@@ -3,6 +3,7 @@ package vista;
 
 import java.awt.Color;
 import java.awt.Cursor;
+import java.awt.Font;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -11,13 +12,15 @@ import javax.swing.JLabel;
 import static javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE;
 
 
+
 public class Ventana extends JFrame{
 
-        JButton jbIngresoDatos, jbConsulta, jbAcercaDe;
-        JLabel jlImg, jlImg1, jlTitu;
+        JButton jbIngresoDatos, jbConsulta, jbAcercaDe, jbVentas;
+        JLabel jlImg, jlImg1,jlImg2, jlTitu, jlBienbenidos;
         
         public Ventana(){
             
+        setTitle("Inventario Dulceria");
         setSize(600, 500);
         setLocationRelativeTo(null);
         setLayout(null);
@@ -34,13 +37,71 @@ public class Ventana extends JFrame{
         
         public void crearGUI() {
         
-        jbIngresoDatos = new JButton("ENTRAR");
-        jbIngresoDatos.setBounds(250, 520, 100, 30);
+       
+          
+        jlTitu = new JLabel("<html>Inventario Dulceria<br></center>");
+        jlTitu.setBackground(new Color(255, 116, 148));
+        jlTitu.setHorizontalAlignment(JLabel.CENTER);
+        jlTitu.setBounds(0, 0, 600, 60);
+        //jlTitu.setBounds(0, 0, 80, 60);// este metodo sirve hubicar en la pantalla 
+        jlTitu.setOpaque(true);// esto sirve para poner el fondo blanco del texto 
+        //jlTitu.setBackground(Color.WHITE);
+        jlTitu.setForeground(Color.WHITE);// estesirve para cambiar el color de la letra
+        jlTitu.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,25)); // sirve para cambiar el tipo de letra
+        
+        
+        add(jlTitu); 
+        
+        jlBienbenidos = new JLabel("<html>Bienvenidos<br></center>");
+        jlBienbenidos.setBounds(300, 60, 160, 60);
+        jlBienbenidos.setFont(new Font("Tomaha", Font.BOLD|Font.BOLD,25));  
+        jlBienbenidos.setForeground(Color.WHITE);
+        add(jlBienbenidos);
+        
+       
+        ImageIcon ii = new ImageIcon(getClass().getResource("../imagenes/Dulceria.png"));  
+        jlImg = new JLabel(ii);
+        jlImg.setBounds(40, 70, 128, 128); // sirve para cetrar la imagen 
+        //jlImg.setBorder(new LineBorder(Color.CYAN));
+        jlImg.setToolTipText(" Endulzate la vida ");
+        add(jlImg);
+        
+        ImageIcon iii = new ImageIcon(getClass().getResource("../imagenes/dulces.png"));
+        jlImg2 = new JLabel(iii);
+        jlImg2.setBounds(260, 130, 256, 256);
+        add(jlImg2);
+        
+        jlImg1 = new JLabel("");
+        jlImg1.setBounds(0, 60, 180, 160);
+        jlImg1.setBackground(new Color(255, 147, 169));
+        jlImg1.setOpaque(true);
+        add(jlImg1);
+        
+        
+        
+        jbIngresoDatos = new JButton("Ingresar Datos");
+        jbIngresoDatos.setBounds(30, 240, 150, 30);
         jbIngresoDatos.setToolTipText("Entrar");
-        //jbEntrar.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        //jbEntrar.addActionListener(lc);
-        //add(jbEntrar);
+        add(jbIngresoDatos);
             
+        
+        jbConsulta = new JButton("Consultar Datos");
+        jbConsulta.setBounds(30, 290, 150, 30);
+        jbConsulta.setToolTipText("Entrar");
+        add(jbConsulta);
+        
+        jbVentas = new JButton("Ventas");
+        jbVentas.setBounds(30, 340, 150, 30);
+        jbVentas.setToolTipText("Entrar");
+        add(jbVentas);
+        
+        
+        jbAcercaDe = new JButton("Acerca De..");
+        jbAcercaDe.setBounds(30, 390, 150, 30);
+        jbAcercaDe.setToolTipText("Entrar");
+        add(jbAcercaDe);
+        
+        
         }
         
     public static void main(String[] args) {
